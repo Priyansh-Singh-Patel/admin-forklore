8b8a3e6f6f5700059f0136b0e1c36232a60d38d3
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-08-29 23:38:05 +0530

    commit

--- app/src/main/java/com/example/adminforklore/AllItemActivity.kt
+++ app/src/main/java/com/example/adminforklore/AllItemActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.adminforklore.adapter.AddItemAdapter
+import com.example.adminforklore.databinding.ActivityAllItemBinding
 
 class AllItemActivity : AppCompatActivity() {
+
+    private val binding: ActivityAllItemBinding by lazy {
+        ActivityAllItemBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_all_item)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val menuFoodName = listOf("Burger", "Pizza", "Samosa", "Chat", "Sandwich", "Momo")
+        val menuItemPrice = listOf("₹50", "₹150", "₹10", "₹50", "₹50", "₹60")
+        val menuImage = listOf(
+            R.drawable.menu1,
+            R.drawable.menu2,
+            R.drawable.menu3,
+            R.drawable.menu4,
+            R.drawable.menu5,
+            R.drawable.menu1
+        )
+
+        val adapter = AddItemAdapter(ArrayList(menuFoodName), ArrayList(menuItemPrice), ArrayList(menuImage))
+        binding.menuRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.menuRecyclerView.adapter = adapter
     }
 }
