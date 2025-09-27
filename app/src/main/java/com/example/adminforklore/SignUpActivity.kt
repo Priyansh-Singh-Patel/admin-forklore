b875714308ac0726391a79a9011dad95f7227186
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-09-28 03:07:15 +0530

    sign up

--- app/src/main/java/com/example/adminforklore/SignUpActivity.kt
+++ app/src/main/java/com/example/adminforklore/SignUpActivity.kt
@@ -1,15 +1,33 @@
 package com.example.adminforklore
 
+import android.accounts.Account
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminforklore.databinding.ActivitySignUpBinding
+import com.example.adminforklore.model.UserModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var userName: String
+    private lateinit var nameOfRestaurant: String
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
     private val binding: ActivitySignUpBinding by lazy {
         ActivitySignUpBinding .inflate(layoutInflater)
     }
@@ -23,14 +41,32 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //initialize Firebase Auth
+        auth = Firebase.auth
+        //initialize Firebase Database
+        database = Firebase.database.reference
+
+
+
         val locationList = arrayOf("Jaipur", "Lucknow", "Delhi", "Mumbai", "Bangalore")
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,locationList)
         val autoCompleteTextView = binding.listOfLocation
         autoCompleteTextView.setAdapter(adapter)
 
-        binding.createAccountButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        binding.createUserButton.setOnClickListener {
+            //getting text from edittext
+            userName = binding.name.text.toString().trim()
+            nameOfRestaurant = binding.restaurantName.text.toString().trim()
+            email = binding.email.text.toString().trim()
+            password = binding.password.text.toString().trim()
+
+            if (userName.isBlank() || nameOfRestaurant.isBlank() || email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Please fill all details", Toast.LENGTH_SHORT).show()
+            } else {
+                createAccount(email,password)
+            }
+
         }
 
         binding.alreadyhavebutton.setOnClickListener {
@@ -38,5 +74,31 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this,"Account created successfully, Please Login", Toast.LENGTH_SHORT).show()
+                saveUserData()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Account creation failed", Toast.LENGTH_SHORT).show()
+                Log.d("Account", "createAccount: Failure", task.exception)
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        userName = binding.name.text.toString().trim()
+        nameOfRestaurant = binding.restaurantName.text.toString().trim()
+        email = binding.email.text.toString().trim()
+        password = binding.password.text.toString().trim()
+
+        val user = UserModel(userName,nameOfRestaurant,email,password)
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        database.child("user").child(userId).setValue(user)
     }
 }
