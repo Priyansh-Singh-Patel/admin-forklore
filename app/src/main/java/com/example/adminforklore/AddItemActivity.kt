ce5c723f5d4e2bef4125f232b942c3b0744d1ae3
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-10-14 21:36:36 +0530

    commit

--- app/src/main/java/com/example/adminforklore/AddItemActivity.kt
+++ app/src/main/java/com/example/adminforklore/AddItemActivity.kt
@@ -1,6 +1,7 @@
 package com.example.adminforklore
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.PickVisualMediaRequest
@@ -10,8 +11,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminforklore.databinding.ActivityAddItemBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class AddItemActivity : AppCompatActivity() {
+
+    //Food Item Details according to AllMenu Model which is based on add_item
+    private lateinit var foodName: String
+    private lateinit var foodPrice: String
+    private var foodImageUri: Uri? = null
+    private lateinit var foodDescription: String
+    private lateinit var foodIngredients: String
+
+    //Firebase variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
     private val binding: ActivityAddItemBinding by lazy {
         ActivityAddItemBinding.inflate(layoutInflater)
     }
@@ -23,6 +38,15 @@ class AddItemActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //initialize Firebase
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        binding.AddItemButton.setOnClickListener {
+            //Get data from the activity
+            //you left here start here
         }
 
         binding.selectImage.setOnClickListener {
